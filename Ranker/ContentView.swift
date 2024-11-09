f96537d4c427782f6589ac53e7668fe720b46c0d
wrdowney <me@wrdowney.com>
2024-11-09 12:18:10 -0500

    basic structure

--- Ranker/ContentView.swift
+++ Ranker/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+struct Item: Identifiable, Codable {
+    var id: UUID = UUID()
+    let title: String
+    let image: String
+    let description: String
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @AppStorage("items") private var savedItems: Data = Data()
+    
+    @State private var items: [Item] = [
+        Item(title: "Item 1", image: "image1", description: "Description 1"),
+        Item(title: "Item 2", image: "image2", description: "Description 2"),
+        Item(title: "Item 3", image: "image3", description: "Description 3")
+    ] {
+        didSet {
+            
         }
-        .padding()
+    }
+    
+    @State private var selectedItems: [Item] = []
+    @State private var isEditModeActive: EditMode = .inactive
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(items) { item in
+                        HStack {
+                            Image(item.image)
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                            
+                            VStack(alignment: .leading) {
+                                Text(item.title)
+                                    .font(.headline)
+                                Text(item.description)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                            
+                            Spacer()
+                            
+                            
+                        }
+                }
+            }
+        }
     }
 }
 
