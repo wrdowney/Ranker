859d64fbc0d856b310c5af326595e7fcf70f3d5e
wrdowney <me@wrdowney.com>
2024-11-10 13:47:12 -0500

    list styling

--- Ranker/Views/EditView.swift
+++ Ranker/Views/EditView.swift
@@ -15,6 +15,7 @@ struct EditView: View {
     @State private var showAddOptionSheet = false
     @State private var rotation: Double = 0.0
     @State private var floatingButtonIsAnimating: Bool = false
+    @State private var addOptionButtonIsAnimating: Bool = false
     
     var body: some View {
         GeometryReader { proxy in
@@ -58,7 +59,7 @@ struct EditView: View {
                     .offset(x: -width * 0.4, y: -height * 0.1)
                 
                 ScrollView(.vertical, showsIndicators: false) {
-                    VStack(alignment: .leading) {
+                    VStack {
                         HStack {
                             Image(systemName: "person")
                                 .resizable()
@@ -67,7 +68,7 @@ struct EditView: View {
                                 .scaledToFit()
                                 .background(.white)
                                 .clipShape(Circle())
-                                .shadow(color: Color(.black), radius: 0, x: 5, y: 5)
+                                .shadow(color: Color(.black), radius: 0, x: 3, y: 3)
                                 .overlay(
                                     Circle()
                                         .stroke(Color.black, lineWidth: 3)
@@ -92,38 +93,50 @@ struct EditView: View {
                         .buttonStyle(.plain)
                         .padding(.horizontal)
                         
-                        Spacer()
-                        List {
+                        LazyVStack {
                             ForEach(listModel.elements) { element in
                                 HStack {
-                                    element.image
+                                    image
                                         .resizable()
-                                        .frame(width: 30, height: 30)
+                                        .scaledToFit()
+                                        .cornerRadius(10)
+                                        .frame(width: 60, height: 60)
+                                        .foregroundColor(.black)
+                                        .padding(.horizontal, 6)
                                     Text(element.title)
-                                        .fontWeight(.bold)
+                                        .font(.system(size: 20, weight: .semibold))
+                                        .foregroundColor(.black)
                                     Spacer()
-                                    Button {
-                                        listModel.elements.removeAll { $0.id == element.id }
                                         
-                                    } label: {
-                                        Image(systemName: "trash")
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.red)
-                                    }
+                                    Image(systemName: "trash")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .foregroundColor(.red)
+                                        .frame(width: 30, height: 30)
+                                        .padding(.horizontal)
+                                        .onTapGesture {
+                                            Task {
+                                                UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                                            }
+                                            listModel.elements.removeAll { $0.id == element.id }
+                                        }
                                 }
-                                .listRowSeparator(.hidden)
-                                .padding()
+                                .padding(2)
                                 .background(.white)
                                 .cornerRadius(10)
-                                .border(Color(.black), width: 3)
-                                .shadow(color: Color(.black), radius: 3, x: -5, y: 5)
-                        
+                                .shadow(color: Color(.black), radius: 0, x: 3, y: 3)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.black, lineWidth: 3)
+                                    )
                             }
-                            .listRowInsets(.init(top: 4, leading: 8, bottom: 4, trailing: 8))
                         }
-                        .listStyle(.plain)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                     }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .padding()
+
+                        
                 }
                 
                 // MARK: Floating add button
@@ -141,16 +154,16 @@ struct EditView: View {
                     .padding()
                     .onTapGesture {
                         Task {
-                            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
                         }
                         withAnimation(.spring(response: 0.15, dampingFraction: 0.3, blendDuration: 0.1)) {
                             floatingButtonIsAnimating.toggle()
                         } completion: {
                             withAnimation(.spring(response: 0.15, dampingFraction: 0.3, blendDuration: 0.1)) {
                                 floatingButtonIsAnimating.toggle()
+                                showAddOptionSheet.toggle()
                             }
                         }
-                        showAddOptionSheet.toggle()
                     }
                     .offset(x: floatingButtonIsAnimating ? 3 : 0, y: floatingButtonIsAnimating ? 3 : 0)
                     .sheet(isPresented: $showAddOptionSheet) {
@@ -182,6 +195,7 @@ struct EditView: View {
                                 image
                                     .resizable()
                                     .scaledToFit()
+                                    .cornerRadius(10)
                                     .frame(width: 60, height: 60)
                                     .foregroundColor(.black)
                                     .padding(.horizontal, 6)
@@ -193,7 +207,17 @@ struct EditView: View {
                                     )
                             }
                             .padding(.horizontal)
-                            
+                            .onChange(of: pictureItem) {
+                                Task {
+                                    if let loaded = try? await pictureItem?.loadTransferable(type: Image.self) {
+                                        image = loaded
+                                    } else {
+                                        image = Image(systemName: "photo")
+                                    }
+                                    
+                                }
+                            }
+                                
                             Text("Add")
                                 .frame(maxWidth: .infinity)
                                 .font(.system(size: 20, weight: .bold))
@@ -201,11 +225,38 @@ struct EditView: View {
                                 .padding()
                                 .background(.white)
                                 .clipShape(Capsule())
-                                .shadow(color: Color(.black), radius: 0, x: 4, y: 4)
+                                .shadow(
+                                    color: Color(.black),
+                                    radius: 0,
+                                    x: addOptionButtonIsAnimating ? 0 : 3,
+                                    y: addOptionButtonIsAnimating ? 0 : 3
+                                )
                                 .overlay(
                                     Capsule()
                                         .stroke(Color.black, lineWidth: 3)
                                     )
+                                .onTapGesture {
+                                    Task {
+                                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                                    }
+                                    withAnimation(.spring(response: 0.15, dampingFraction: 0.3, blendDuration: 0.1)) {
+                                        addOptionButtonIsAnimating.toggle()
+                                    } completion: {
+                                        withAnimation(.spring(response: 0.15, dampingFraction: 0.3, blendDuration: 0.1)) {
+                                            addOptionButtonIsAnimating.toggle()
+                                        } completion: {
+                                            listModel.elements.append(ElementModel(title: title, image: image))
+                                            title = ""
+                                            pictureItem = nil
+                                            image = Image(systemName: "photo")
+                                            showAddOptionSheet.toggle()
+                                        }
+                                    }
+                                }
+                                .offset(
+                                    x: addOptionButtonIsAnimating ? 3 : 0,
+                                    y: addOptionButtonIsAnimating ? 3 : 0
+                                )
                         }
                         .padding()
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -213,7 +264,6 @@ struct EditView: View {
                         .presentationDetents([.fraction(0.4)])
                         
                     }
-                        
             }
         }
     }
